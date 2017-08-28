35a36222a7c5aefc491025942d5f24541ef9c5a7
JooYoungHeo <jyhur900226@naver.com>
2017-08-28 12:37:50 +0900

    [작업] instagram oauth 구현 완료

--- routes/authToInsta.js
+++ routes/authToInsta.js
@@ -6,15 +6,42 @@ const config = require(path.join(process.cwd(), 'config/config'));
 
 const Koa = new koa();
 
-const instaAuthUrl = 'https://api.instagram.com/oauth/authorize/?client_id=' + config.client_id +
+const instaReqCodeUrl = 'https://api.instagram.com/oauth/authorize/?client_id=' + config.client_id +
 												'&redirect_uri=' + config.redirect_url + '&response_type=code';
+const instaReqTokenUrl = 'https://api.instagram.com/oauth/access_token';
 
 Koa.use(route.get('/', async (ctx, next) => {
-	ctx.redirect(instaAuthUrl);
+	ctx.redirect(instaReqCodeUrl);
 }));
 
-Koa.use(route.get('/callback', async(ctx, next) => {
-	ctx.body = 'hi';
+Koa.use(route.get('/callback', async (ctx, next) => {
+	let code = ctx.query.code;
+	let requestForm = {
+		client_id: config.client_id,
+		client_secret: config.client_secret,
+		grant_type: 'authorization_code',
+		redirect_uri: config.redirect_url,
+		code: code
+	};
+
+	await getToken(requestForm).then(result => {
+		ctx.body = result;
+	}).catch(err => {
+		ctx.body = err;
+	});
+
 }));
+
+function getToken(requestForm) {
+	return new Promise((resolve, reject) => {
+		request.post({url: instaReqTokenUrl, formData: requestForm}, (err, httpResponse, body) => {
+			if(!err) {
+				resolve(body);
+			} else {
+				reject(err);
+			}
+		});
+	});
+}
 
 module.exports = Koa;
