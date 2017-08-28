7e795ab6e2de8783f9c0cbd233bd2ce1f78d1cd2
JooYoungHeo <jyhur900226@naver.com>
2017-08-28 11:16:09 +0900

    [작업] insta oauth 구현 ( ~ getCode)

--- routes/authToInsta.js
+++ routes/authToInsta.js
@@ -1,10 +1,20 @@
 const route = require('koa-route');
 const koa = require('koa');
+const path = require('path');
+const request = require('request');
+const config = require(path.join(process.cwd(), 'config/config'));
 
 const Koa = new koa();
 
+const instaAuthUrl = 'https://api.instagram.com/oauth/authorize/?client_id=' + config.client_id +
+												'&redirect_uri=' + config.redirect_url + '&response_type=code';
+
 Koa.use(route.get('/', async (ctx, next) => {
-	ctx.body = {test: 'qwer', test2: 'zxcv', test3: 'ppppp'};
+	ctx.redirect(instaAuthUrl);
+}));
+
+Koa.use(route.get('/callback', async(ctx, next) => {
+	ctx.body = 'hi';
 }));
 
 module.exports = Koa;
