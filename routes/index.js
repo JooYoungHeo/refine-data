7d333946e1443cec4b5a8326e049db94cdfcad3a
JooYoungHeo <jyhur900226@naver.com>
2017-08-30 14:42:58 +0900

    [작업] 미사용 및 테스트 파일 제거

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,12 @@ const mount = require('koa-mount');
 
 const Koa = new koa();
 
-const authToInsta = require('./authToInsta');
 const insta = require('./insta');
 const riot = require('./riot');
-const routerTest = require('./routerTest');
 const facebook = require('./facebook');
 
-// Koa.use(mount('/insta/auth', authToInsta));
 Koa.use(mount('/insta', insta));
 Koa.use(mount('/riot', riot));
-Koa.use(mount('/test', routerTest));
 Koa.use(mount('/facebook', facebook));
 
 module.exports = Koa;
