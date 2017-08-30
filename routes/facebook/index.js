a96d66b2ccb4153ff66f162ea57db01848e56e51
JooYoungHeo <jyhur900226@naver.com>
2017-08-30 21:01:13 +0900

    [작업] facebook myInfo.js routing 작업

--- routes/facebook/index.js
+++ routes/facebook/index.js
@@ -1,9 +1,11 @@
 const koa = require('koa');
 const mount = require('koa-mount');
 const login = require('./login');
+const me = require('./myInfo');
 
 const Koa = new koa();
 
 Koa.use(mount('/', login));
+Koa.use(mount('/me', me));
 
 module.exports = Koa;
