5a56e2b7abedd7fe8ebd140a32058f8430c358b7
JooYoungHeo <jyhur900226@naver.com>
2017-08-30 21:41:20 +0900

    [작업] facebook - get myInfo api realize

--- routes/facebook/myInfo.js
+++ routes/facebook/myInfo.js
@@ -1,12 +1,38 @@
 const koa = require('koa');
 const router = require('koa-router');
+const path = require('path');
+const fs = require('fs');
+const request = require('request');
+const config = require(path.join(process.cwd(), 'config/config'));
+const infoFile = path.join(process.cwd(), '../oauthInfo.json');
+const facebookInfo = JSON.parse((JSON.parse(fs.readFileSync(infoFile))).facebook);
 
 const Koa = new koa();
 const Router = new router();
 
 Router.get('/', async (ctx, next) => {
-  ctx.body = 'ok';
+
+  let requestObject = {
+    url: 'https://graph.facebook.com/me?fields=id,name,picture',
+    headers: {
+      Authorization: `Bearer ${facebookInfo.access_token}`
+    }
+  };
+
+  await doRequest(requestObject).then(result => {
+    ctx.body = result;
+  }).catch(err => {
+    ctx.body = err;
+  });
 });
+
+function doRequest(requestObject) {
+  return new Promise((resolve, reject) => {
+    request.get(requestObject, (err, httpResponse, body) => {
+      err? reject(err): resolve(body);
+    });
+  });
+}
 
 Koa.use(Router.routes());
 
