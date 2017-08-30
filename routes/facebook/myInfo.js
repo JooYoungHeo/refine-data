18a51d1f62f84cd4b3fff74543829c90a8a79a3e
JooYoungHeo <jyhur900226@naver.com>
2017-08-30 21:56:38 +0900

    [작업] myInfo request - util 함수로 변경

--- routes/facebook/myInfo.js
+++ routes/facebook/myInfo.js
@@ -2,9 +2,11 @@ const koa = require('koa');
 const router = require('koa-router');
 const path = require('path');
 const fs = require('fs');
-const request = require('request');
-const config = require(path.join(process.cwd(), 'config/config'));
-const infoFile = path.join(process.cwd(), '../oauthInfo.json');
+
+const cwd = process.cwd();
+const util = require(path.join(cwd, 'util'));
+const config = require(path.join(cwd, 'config/config'));
+const infoFile = path.join(cwd, '../oauthInfo.json');
 const facebookInfo = JSON.parse((JSON.parse(fs.readFileSync(infoFile))).facebook);
 
 const Koa = new koa();
@@ -13,26 +15,19 @@ const Router = new router();
 Router.get('/', async (ctx, next) => {
 
   let requestObject = {
+    method: 'GET',
     url: 'https://graph.facebook.com/me?fields=id,name,picture',
     headers: {
       Authorization: `Bearer ${facebookInfo.access_token}`
     }
   };
 
-  await doRequest(requestObject).then(result => {
+  await util.requestOriginApi(requestObject).then(result => {
     ctx.body = result;
   }).catch(err => {
     ctx.body = err;
   });
 });
-
-function doRequest(requestObject) {
-  return new Promise((resolve, reject) => {
-    request.get(requestObject, (err, httpResponse, body) => {
-      err? reject(err): resolve(body);
-    });
-  });
-}
 
 Koa.use(Router.routes());
 
