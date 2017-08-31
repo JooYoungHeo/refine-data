a77f2bb23f0a64fed950064b91184e29daa61197
JooYoungHeo <jyhur900226@naver.com>
2017-08-31 15:54:02 +0900

    [작업] daum cafe query search realize

--- routes/kakao/search.js
+++ routes/kakao/search.js
@@ -1,11 +1,34 @@
 const koa = require('koa');
 const router = require('koa-router');
+const path = require('path');
+
+const cwd = process.cwd();
+const {requestOriginApi} = require(path.join(cwd, 'util'));
+const config = require(path.join(cwd, 'config/config'));
 
 const Koa = new koa();
 const Router = new router();
 
 Router.get('/', async (ctx, next) => {
-  ctx.body = 'ok';
+  let apiKey = config.kakao.apiKey;
+  let query = ctx.query.query;
+  let queryString = {
+    query: query
+  };
+  let requestObject = {
+    method: 'GET',
+    headers: {
+      Authorization: `KakaoAK ${apiKey}`
+    },
+    url: 'https://dapi.kakao.com/v2/search/cafe',
+    qs: queryString
+  };
+
+  await requestOriginApi(requestObject).then(result => {
+    ctx.body = result;
+  }).catch(err => {
+    ctx.body = err;
+  });
 });
 
 Koa.use(Router.routes());
