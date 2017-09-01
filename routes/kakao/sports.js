72ec6e4724507a3cfb3c78f5e2a86743cd722604
JooYoungHeo <jyhur900226@naver.com>
2017-09-01 19:36:37 +0900

    [작업] sports list 조회 api 구현, 테스트

--- routes/kakao/sports.js
+++ routes/kakao/sports.js
@@ -1,12 +1,31 @@
 const koa = require('koa');
 const router = require('koa-router');
+const mongoose = require('mongoose');
+const path = require('path');
 
 const Koa = new koa();
 const Router = new router();
 
+let Sports = require(path.join(process.cwd(), 'models/sports'));
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/test', {useMongoClient: true});
+
 Router.get('/', async (ctx, next) => {
-  ctx.body = 'ok';
+  await findSports().then(result => {
+    ctx.body = result;
+  }).catch(err => {
+    ctx.body = err;
+  });
 });
+
+function findSports() {
+  return new Promise((resolve, reject) => {
+    Sports.find((err, item) => {
+      err? reject(err): resolve(item);
+    });
+  });
+}
 
 Koa.use(Router.routes());
 
