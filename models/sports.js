51b7ac7cda83d844ba7d2e7eaffafd9a257d3e54
JooYoungHeo <jyhur900226@naver.com>
2017-09-01 19:09:47 +0900

    [작업] Sports model 작성 (for mongoose)

---
+++ models/sports.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+let sportsSchema = new Schema({
+  cafename: String,
+  contents: String,
+  datetime: String,
+  thumbnail: String,
+  title: String,
+  url: String,
+  category: String
+});
+
+module.exports = mongoose.model('sports', sportsSchema);
