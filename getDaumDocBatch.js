694799cf964751fac6df0a54b9d60eadc12bc81b
JooYoungHeo <jyhur900226@naver.com>
2017-09-06 19:30:12 +0900

    [작업] query loop 추가 (in batch)

--- getDaumDocBatch.js
+++ getDaumDocBatch.js
@@ -2,63 +2,64 @@ const request = require('request');
 const path = require('path');
 const cron = require('node-cron');
 const mongoose = require('mongoose');
+const fs = require('fs');
 
 const cwd = process.cwd();
 const config = require(path.join(cwd, 'config/config'));
 const {requestOriginApi} = require(path.join(cwd, 'util'));
 
-let Sports = require(path.join(cwd, 'models/sports'));
-
-mongoose.Promise = global.Promise;  //mongoose use bluebird promise -> native es6 promise (change)
-mongoose.connect('mongodb://localhost/test', {useMongoClient: true});
-
+let {Sports} = require(path.join(cwd, 'models'));
+let sportsList = (fs.readFileSync(path.join(cwd, '../sports.txt'), 'utf-8')).split('\n');
 let apiKey = config.kakao.apiKey;
-let query = '축구';
-let size = 50;
-let requestObject = {
-  method: 'GET',
-  url: 'https://dapi.kakao.com/v2/search/cafe',
-  headers: {
-    Authorization: `KakaoAK ${apiKey}`
-  },
-  qs: {
-    query: query,
-    sort: 'recency',
-    size: size
-  }
-};
 
 cron.schedule('35 */1 * * *', () => {
   let date = new Date();
 
-  requestOriginApi(requestObject).then(result => {
-    let jsonResult = JSON.parse(result);
-    let docs = jsonResult.documents;
+  for (let i = 0 ; i < sportsList.length ; i++) {
+    let query = sportsList[i];
+    let size = 50;
+    let requestObject = {
+      method: 'GET',
+      url: 'https://dapi.kakao.com/v2/search/cafe',
+      headers: {
+        Authorization: `KakaoAK ${apiKey}`
+      },
+      qs: {
+        query: query,
+        sort: 'recency',
+        size: size
+      }
+    };
 
-    console.log('json result: ' + docs.length);
+    requestOriginApi(requestObject).then(result => {
+      let jsonResult = JSON.parse(result);
+      let docs = jsonResult.documents;
 
-    for (let i = 0 ; i < docs.length ; i++) {
-      let doc = docs[i];
-      let item = new Sports();
+      console.log('json result: ' + docs.length);
 
-      item._id = doc.url;
-      item.cafename = doc.cafename;
-      item.contents = doc.contents;
-      item.datetime = doc.datetime;
-      item.thumbnail = doc.thumbnail;
-      item.title = doc.title;
-      item.url = doc.url;
-      item.category = query;
+      for (let i = 0 ; i < docs.length ; i++) {
+        let doc = docs[i];
+        let item = new Sports();
 
-      item.save(err => {
-        if(err) {
-          console.log(date.toLocaleString());
-          console.log('duplicate url: ' + doc.url);
-        }
-      });
-    }
+        item._id = doc.url;
+        item.cafename = doc.cafename;
+        item.contents = doc.contents;
+        item.datetime = doc.datetime;
+        item.thumbnail = doc.thumbnail;
+        item.title = doc.title;
+        item.url = doc.url;
+        item.category = query;
 
-  }).catch(err => {
-    console.error(err);
-  });
+        item.save(err => {
+          if(err) {
+            console.log(date.toLocaleString());
+            console.log('duplicate url: ' + doc.url);
+          }
+        });
+      }
+
+    }).catch(err => {
+      console.error(err);
+    });
+  }
 }).start();
