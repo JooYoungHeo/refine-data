df1478dc11140105629f8971694b194f860d70f7
JooYoungHeo <jyhur900226@naver.com>
2017-09-06 14:29:18 +0900

    [작업] mongoose 조회 - paging 처리 추가

--- routes/kakao/sports.js
+++ routes/kakao/sports.js
@@ -2,6 +2,7 @@ const koa = require('koa');
 const router = require('koa-router');
 const mongoose = require('mongoose');
 const path = require('path');
+const _ = require('lodash');
 
 const Koa = new koa();
 const Router = new router();
@@ -9,16 +10,24 @@ const Router = new router();
 let {Sports} = require(path.join(process.cwd(), 'models'));
 
 Router.get('/', async (ctx, next) => {
-  await findSports().then(result => {
+  let skip = ctx.query.page;
+
+  if (_.isNil(skip) || typeof Number(skip) !== 'number') {
+    skip = 1;
+  }
+
+  let skipNumber = (skip - 1) * 10;
+
+  await findSports(skipNumber).then(result => {
     ctx.body = result;
   }).catch(err => {
     ctx.body = err;
   });
 });
 
-function findSports() {
+function findSports(skip) {
   return new Promise((resolve, reject) => {
-    Sports.find((err, item) => {
+    Sports.find().skip(skip).limit(10).exec((err, item) => {
       err? reject(err): resolve(item);
     });
   });
